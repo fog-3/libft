c732a59bc8678b35f541747cc7b060003c23498d
Fernando <Fog-03@hotmail.com>
2023-11-30 09:16:45 +0100

    Subiendo memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,41 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fernando <fernando@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/30 08:07:37 by fernando          #+#    #+#             */
-/*   Updated: 2023/11/30 09:13:00 by fernando         ###   ########.fr       */
+/*   Created: 2023/11/30 09:03:44 by fernando          #+#    #+#             */
+/*   Updated: 2023/11/30 09:15:47 by fernando         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <string.h>
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2 , size_t n)
 {
+	char	*str1;
+	char	*str2;
 	size_t	i;
-
+	
 	i = 0;
-	while ((s1[i] || s2[i]) && n > i)
+	str1 = (char *)s1;
+	str2 = (char *)s2;
+	while ((str1[i] || str1[i]) && i < n)
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
 		i++;
 	}
 	return (0);
 }
 
-/* int main(void)
+/* int	main(void)
 {
 	char    s1[] = "hola buenas";
 	char    s2[] = "hola buenas";
 	int     res;
 
-	res = ft_strncmp(s1, s2, 20);
+	res = ft_memcmp(s1, s2, 13);
 	printf("res: %d\n", res);
-	res = strncmp(s1, s2, 20);
+	res = memcmp(s1, s2, 13);
 	printf("res: %d\n", res);
 	return (0);
 } */
