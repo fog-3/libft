b8bdb1e321a0307bb95601fb5585c33d031dded4
Fernando <Fog-03@hotmail.com>
2023-11-30 08:42:42 +0100

    Subiendo strncmp con norminette

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: fernando <fernando@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 21:13:00 by fosuna-g          #+#    #+#             */
-/*   Updated: 2023/11/30 08:33:19 by fernando         ###   ########.fr       */
+/*   Updated: 2023/11/30 08:42:05 by fernando         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,6 @@ void	*ft_memset(void *s, int c, size_t len);
 void	bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-int	ft_strncmp(const char *s1,const char *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
