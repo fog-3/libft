1ad334fdcc85cd95efae342e41e8a065570224d5
Fernando <Fog-03@hotmail.com>
2023-12-01 19:59:25 +0100

    Subiendo strrchr correción

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fosuna-g <fosuna-g@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fernando <fernando@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 13:05:44 by fernando          #+#    #+#             */
-/*   Updated: 2023/12/01 13:37:21 by fosuna-g         ###   ########.fr       */
+/*   Updated: 2023/12/01 19:58:10 by fernando         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,13 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	int		bol;
 	char	*res;
 
-	bol = 0;
 	res = 0;
 	while (*s)
 	{
 		if (*s == (unsigned char)c)
-		{
-			bol = 1;
 			res = (char *)s;
-		}
 		++s;
 	}
 	if ((char)c == '\0')
