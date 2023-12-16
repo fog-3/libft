0278766e5c4851a31cc2255f23954a5fb9e11eb6
Fernando <Fog-03@hotmail.com>
2023-12-16 23:41:23 +0100

    Subiendo ft_lstadd_front

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: fernando <fernando@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 21:13:00 by fosuna-g          #+#    #+#             */
-/*   Updated: 2023/12/16 23:04:13 by fernando         ###   ########.fr       */
+/*   Updated: 2023/12/16 23:24:12 by fernando         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,5 +58,6 @@ void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
 
 #endif
