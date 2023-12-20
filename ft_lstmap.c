16dcc3f38e722c5724572d3ef0c778a4ef673707
Fernando <Fog-03@hotmail.com>
2023-12-20 09:48:57 +0100

    Subiendo libft terminada

---
+++ ft_lstmap.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fernando <fernando@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/20 08:00:22 by fernando          #+#    #+#             */
+/*   Updated: 2023/12/20 09:48:17 by fernando         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	int		cnt;
+	t_list	*res;
+	t_list	*aux;
+
+	if (!lst)
+		return (0);
+	cnt = ft_lstsize(lst);
+	aux = 0;
+	while (cnt > 0)
+	{
+		res = ft_lstnew(f(lst->content));
+		if (!res)
+		{
+			ft_lstclear(&res, del);
+			return (0);
+		}
+		ft_lstadd_back(&aux, res);
+		lst = lst->next;
+		cnt--;
+	}
+	return (aux);
+}
